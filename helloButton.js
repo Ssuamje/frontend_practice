bdc9dcfd62648cfd72f129ec5fc8b2de0200735b
Ssuamje <side2850@naver.com>
2023-08-19 19:13:12 +0900

    입력받은 숫자값 출력해보기

--- helloButton.js
+++ helloButton.js
@@ -1,3 +1,8 @@
-function buttonClick() {
-    alert("Hello World!");
+let randomAnswer = Math.floor(Math.random() * 100) + 1;
+let guessCount = 0;
+
+function submitGuess() {
+    var userGuess = document.querySelector(".guesses").value;
+    guessCount++;
+    console.log(userGuess);
 }
