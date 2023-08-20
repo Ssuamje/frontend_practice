7fd18d77f8bda26b774288f1e6cd46b4e3d5cd67
Ssuamje <side2850@naver.com>
2023-08-20 11:45:25 +0900

    commit finder 예제 작성

---
+++ react/JS/commit_log_finder/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [commitPrefix, setCommitPrefix] = useState(null);
+  const [commitLogs, setCommitLogs] = useState(null);
+
+  useEffect(() => {
+    async function getCommitLogs() {
+      const response = await fetch('https://api.github.com/repos/sveltejs/svelte/commits');
+      const commitLogs = await response.json();
+      setCommitLogs(commitLogs);
+    }
+    getCommitLogs();
+    console.log(commitLogs);
+  }, []) // depends on, 해당 요소가 변경됨에 따라 실행됨
+
+  function setCommitPrefixHandler({target: {value}}) { // destructing, 해체 할당 문법
+    setCommitPrefix(value);
+  }
+
+  return (
+    <>
+      <h1>Commit Filter</h1>
+      <input type="text" name='commitFilterInput' id='commitFilterInput'  onChange={setCommitPrefixHandler}/>
+      <h2>{commitPrefix}</h2>
+      <ul>
+        {
+          commitLogs && commitLogs.filter(({commit: {message}}) => {
+            const bMatch = commitPrefix && RegExp(`^${commitPrefix}.+`).test(message);
+            return bMatch;
+          }).map(({commit: {message}}, idx) => {
+            return <li key={idx}>{message}</li>
+          })
+        }
+      </ul>
+    </>
+  )
+}
+
+export default App
