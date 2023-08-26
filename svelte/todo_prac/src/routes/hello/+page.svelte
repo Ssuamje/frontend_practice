3ef08d463b8325ab05619d3b574c36caf4e1979c
Ssuamje <side2850@naver.com>
2023-08-26 22:26:02 +0900

    간단한 이름 써보기

---
+++ svelte/todo_prac/src/routes/hello/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { name } from "./stores";
+
+    onMount(() => {
+        $name = '상제';
+        console.log(`${$name} ㅎㅇ`);
+    });
+
+    let userInput = '';
+
+    function updateName() {
+        $name = userInput;
+    }
+
+</script>
+
+<h1>안녕! {$name}</h1>
+<input bind:value={userInput} placeholder="이름">
+<button on:click={updateName}></button>
